50772ee644141f46ac4af71e4b59482539d25c3a
tracer-ics2024 <tracer@njuics.org>
2024-09-12 21:42:18 +0800

    >  compile NEMU 231220003 徐梓鸣 Linux carottx-VirtualBox 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:42:18 up 34 min,  4 users,  load average: 0.35, 0.23, 0.36

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -1,5 +1,5 @@
 /***************************************************************************************
-* Copyright (c) 2014-2022 Zihao Yu, Nanjing University
+Copyright* Copyright (c) 2014-2022 Zihao Yu, Nanjing University
 *
 * NEMU is licensed under Mulan PSL v2.
 * You can use this software according to the terms and conditions of the Mulan PSL v2.
@@ -54,6 +54,18 @@ static int cmd_q(char *args) {
   return -1;
 }
 
+static int cmd_si(char *args){
+	char* arg = strtok(NULL, " ");
+	if (arg == NULL){
+		cpu_exec(1);		
+ 	}	
+	else{ 
+		int cmd_cnt = strtol(arg, NULL, 10);
+		cpu_exec(cmd_cnt);
+	}		
+	return 0;
+}
+
 static int cmd_help(char *args);
 
 static struct {
@@ -64,6 +76,7 @@ static struct {
   { "help", "Display information about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
+  { "si", "Single execute of the program", cmd_si}
 
   /* TODO: Add more commands */
 
