f22b20fdd799e49f2d858b129e3766ed4166a0bd
tracer-ics2024 <tracer@njuics.org>
2024-09-18 16:58:36 +0800

    >  compile NEMU 231220003 徐梓鸣 Linux carottx-VirtualBox 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:58:36 up 33 min,  1 user,  load average: 1.13, 1.01, 0.89

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -126,8 +126,8 @@ static bool make_token(char *e) {
 						tokens[nr_token++].type = rules[i].token_type;
 						break;
           case TK_REG:
-            strncpy(tokens[nr_token].str, substr_start, substr_len);
-            tokens[nr_token].str[substr_len] = '\0';
+            strncpy(tokens[nr_token].str, substr_start+1, substr_len-1);
+            tokens[nr_token].str[substr_len-1] = '\0';
 						tokens[nr_token++].type = rules[i].token_type;
             break;
           case TK_HEX:
