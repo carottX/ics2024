6615b641c4af0caf9ae8d64fe901f935ab2ae9ac
tracer-ics2024 <tracer@njuics.org>
2024-09-18 16:55:13 +0800

    >  compile NEMU 231220003 徐梓鸣 Linux carottx-VirtualBox 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:55:13 up 29 min,  1 user,  load average: 1.72, 1.29, 0.95

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -31,7 +31,7 @@ void isa_reg_display() {
 
 word_t isa_reg_str2val(const char *s, bool *success) {
   for(int idx = 0; idx < 32; ++idx){
-    if(reg_name(idx) == s) {
+    if(reg_name(idx) == (s+1)) {
       *success = true;
       return gpr(idx);
     }
