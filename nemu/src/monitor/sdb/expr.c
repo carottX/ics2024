9438d6880f9ccbc9129cd8878eac90f3a973d41b
tracer-ics2024 <tracer@njuics.org>
2024-09-20 16:01:01 +0800

    >  compile NEMU 231220003 徐梓鸣 Linux carottx-VirtualBox 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:01:01 up 9 min,  1 user,  load average: 0.65, 0.60, 0.41

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -52,7 +52,7 @@ static struct rule {
 	{"-", TK_SUB},
 	{"/", TK_DIV},
   {"0x[0-9]+", TK_HEX},
-  {"\\$(\\$0|ra|sp|tp|gp|pc|[a-z][0-9]+)", TK_REG},
+  {"\\$(0|ra|sp|tp|gp|pc|[a-z][0-9]+)", TK_REG},
   {"!=", TK_NEQ},
   {"&&", TK_AND},
 	{"[0-9]+", TK_NUM},
