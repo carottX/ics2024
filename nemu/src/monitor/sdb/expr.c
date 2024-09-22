74ab98debc1740159ed29d8799cac2814f11bcfc
tracer-ics2024 <tracer@njuics.org>
2024-09-22 16:11:01 +0800

    >  compile NEMU 231220003 徐梓鸣 Linux carottx-VirtualBox 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:11:01 up 2 min,  1 user,  load average: 1.17, 0.93, 0.39

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -160,6 +160,10 @@ static bool make_token(char *e) {
       if(e[position]>='a' && e[position]<='z') {
         if(e[position+1] >= 'a' && e[position+1]<='z') assert(0);
         if(e[position+1] >= '0' && e[position+1]<='9') assert(0);
+        if(e[position+1] == ' ') assert(0);
+        if(e[position+1] == '\0') assert(0);
+        if(position && e[position-1] >= 'a' && e[position-1] <= 'z') assert(0);
+        if(position && e[position-1] >= '0' && e[position-1] <= '9') assert(0);
         assert(0);
       }
       return false;
