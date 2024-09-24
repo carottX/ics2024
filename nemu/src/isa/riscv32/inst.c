bb8f200f30b5abad06c659ce4b7dc460a1440eab
tracer-ics2024 <tracer@njuics.org>
2024-09-24 18:37:33 +0800

    >  compile NEMU 231220003 徐梓鸣 Linux carottx-VirtualBox 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:37:33 up 9 min,  1 user,  load average: 0.27, 0.21, 0.12

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -33,7 +33,7 @@ enum {
 #define immU() do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while(0)
 #define immS() do { *imm = SEXT(((BITS(i, 31, 25) << 5) | BITS(i, 11, 7)), 12); } while(0)
 #define immJ() do { uint32_t ___x = ((((BITS(i,31,31) << 8) | BITS(i,19, 12))<<1) | BITS(i, 20, 20)); \
-*imm = SEXT(((___x<<10) | BITS(i,30,21)) << 1, 20);} while(0)
+*imm = SEXT(((___x<<10) | BITS(i,30,21)) << 1, 21);} while(0)
 #define immB() do { uint32_t ___x = ((BITS(i, 31, 31) << 1) | BITS(i, 7, 7));\
 *imm = SEXT((((((___x<<6) | BITS(i,30,25)) << 4) | BITS(i,11,8))<<1), 13);} while(0)
 
