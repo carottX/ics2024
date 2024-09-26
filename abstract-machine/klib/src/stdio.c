1b01c02a2dd56a04e9434e898020921c923c90a6
tracer-ics2024 <tracer@njuics.org>
2024-09-26 11:31:54 +0800

    >  compile NEMU 231220003 徐梓鸣 Linux carottx-VirtualBox 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:31:54 up  1:19,  1 user,  load average: 1.51, 0.78, 0.47

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -14,7 +14,44 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  size_t i = 0;
+  char c;
+  va_list argptr;
+  va_start(argptr, fmt);
+  while((c=*(fmt++))){
+    if(c=='%'){
+      c=*(fmt++);
+      switch(c){
+        case 'd':
+          int tmp = va_arg(argptr, int);
+          int cnt = 0, ttmp = tmp;
+          while(ttmp){
+            cnt++, ttmp/=10;
+          }
+          int cnt2 = 0;
+          while(tmp){
+            ++cnt2;
+            out[i+cnt-cnt2] = tmp%10 + '0';
+            tmp/=10;
+          }
+          i+=cnt;
+          break;
+        case 's':
+          char* s = va_arg(argptr, char*);
+          strcpy(out+i, s);
+          i += strlen(s);
+          break;
+        default:
+          return i; //Error!
+      }
+    }
+    else{
+      out[i] = c;
+      ++i;
+    }
+  }
+  va_end(argptr);
+  return 0;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
