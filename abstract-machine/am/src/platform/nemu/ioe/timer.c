433e0d887265e77966d19f40c47397c70a448e70
tracer-ics2024 <tracer@njuics.org>
2024-10-03 16:09:16 +0800

    >  compile NEMU 231220003 徐梓鸣 Linux carottx-VirtualBox 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:09:16 up  4:14,  1 user,  load average: 0.67, 0.32, 0.29

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -6,8 +6,9 @@ void __am_timer_init() {
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  printf("%u\n", inl(RTC_ADDR));
-  uptime->us = 0;
+  // printf("%u\n", inl());
+  uptime->us = inl(RTC_ADDR);
+  outl(RTC_ADDR, uptime->us+1);
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
