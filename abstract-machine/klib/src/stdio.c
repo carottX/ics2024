bb0432f1342b2267aa12b179df7998eb3fd78332
tracer-ics2024 <tracer@njuics.org>
2024-10-03 16:38:19 +0800

    >  compile NEMU 231220003 徐梓鸣 Linux carottx-VirtualBox 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:38:19 up  4:43,  1 user,  load average: 0.13, 0.42, 0.45

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -38,6 +38,7 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
           }
           int cnt2 = 0;
           cnt = (width==-1?cnt:(cnt>width?cnt:width));
+          putch(padding);
           for(int ii=0;ii<cnt;++ii) out[i+ii] = padding;
           while(tmp){
             ++cnt2;
