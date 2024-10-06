e01a4589186365a3397d09f5e7232a4edcae02d3
tracer-ics2024 <tracer@njuics.org>
2024-10-06 17:26:56 +0800

    >  compile NEMU 231220003 徐梓鸣 Linux carottx-VirtualBox 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:26:56 up 36 min,  1 user,  load average: 0.43, 0.56, 0.51

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -74,6 +74,7 @@ static inline void update_screen() {
 void vga_update_screen() {
   // TODO: call `update_screen()` when the sync register is non-zero,
   // then zero out the sync register
+  // bool sync = io_read(AM_GPU_CONFIG).
 }
 
 void init_vga() {
