43c24f102fcf04b293aa9cb8a5822c12f357ac2b
tracer-ics2024 <tracer@njuics.org>
2024-10-06 12:56:09 +0800

    >  compile NEMU 231220003 徐梓鸣 Linux carottx-VirtualBox 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:56:09 up  1:07,  1 user,  load average: 0.46, 0.54, 0.53

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -54,19 +54,21 @@ void init_map() {
 
 word_t map_read(paddr_t addr, int len, IOMap *map) {
   assert(len >= 1 && len <= 8);
-  IFDEF(CONFIG_DTRACE,printf("Visiting:%s\n",map->name));
+  IFDEF(CONFIG_DTRACE,printf("Reading %s:address=0x%x\n",map->name,addr));
   check_bound(map, addr);
   paddr_t offset = addr - map->low;
   invoke_callback(map->callback, offset, len, false); // prepare data to read
   word_t ret = host_read(map->space + offset, len);
-  IFDEF(CONFIG_DTRACE,printf("Finished visiting:%s\n", map->name));
+  IFDEF(CONFIG_DTRACE,printf("Finished reading %s:address=0x%x\n", map->name, addr));
   return ret;
 }
 
 void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
   assert(len >= 1 && len <= 8);
+  IFDEF(CONFIG_DTRACE,printf("Writing %s:address=%x\n",map->name,addr));
   check_bound(map, addr);
   paddr_t offset = addr - map->low;
   host_write(map->space + offset, len, data);
   invoke_callback(map->callback, offset, len, true);
+  IFDEF(CONFIG_DTRACE,printf("Finished writing %s:address=%x\n", map->name, addr));
 }
