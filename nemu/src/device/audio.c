a6d6cab5576944e700016787e9741596f0260e19
tracer-ics2024 <tracer@njuics.org>
2024-10-07 06:26:03 +0800

    >  compile NEMU 231220003 徐梓鸣 Linux carottx-VirtualBox 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  06:26:03 up  1:57,  1 user,  load average: 1.18, 1.02, 0.65

--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -41,7 +41,7 @@ static void audio_read(uint8_t *stream, int len){
   uint32_t cnt_t = size-writep;
   if(cnt_t >= rlen) SDL_MixAudio(stream, sbuf+writep, rlen, SDL_MIX_MAXVOLUME);
   else {
-    printf("len=%d count=%d size=%d writep=%d cnt_t=%d\n",len, count,size,writep,cnt_t);
+    // printf("len=%d count=%d size=%d writep=%d cnt_t=%d\n",len, count,size,writep,cnt_t);
     SDL_MixAudio(stream, sbuf+writep, cnt_t, SDL_MIX_MAXVOLUME);
     SDL_MixAudio(stream+cnt_t, sbuf, rlen-cnt_t, SDL_MIX_MAXVOLUME);
   }
