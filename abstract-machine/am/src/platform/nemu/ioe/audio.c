6691da5a3a736e9094970ce1b180503357929295
tracer-ics2024 <tracer@njuics.org>
2024-10-07 05:12:26 +0800

    >  compile NEMU 231220003 徐梓鸣 Linux carottx-VirtualBox 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  05:12:26 up 43 min,  1 user,  load average: 0.91, 0.97, 0.77

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -45,11 +45,12 @@ void __am_audio_ctrl(AM_AUDIO_CTRL_T *ctrl) {
   outl(AUDIO_SAMPLES_ADDR, ctrl->samples);
   outl(AUDIO_CHANNELS_ADDR, ctrl->channels);
   outl(AUDIO_FREQ_ADDR, ctrl->freq);
+  __am_audio_init();
+
 }
 
 void __am_audio_status(AM_AUDIO_STATUS_T *stat) {
   stat->count = inl(AUDIO_COUNT_ADDR);
-  __am_audio_init();
 }
 
 void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
