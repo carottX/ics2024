b9f84654f43af2330bb4db46e68182f16e6bd2b1
tracer-ics2024 <tracer@njuics.org>
2024-10-07 06:35:18 +0800

    >  compile NEMU 231220003 徐梓鸣 Linux carottx-VirtualBox 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  06:35:18 up  2:06,  1 user,  load average: 0.32, 0.47, 0.58

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -14,12 +14,13 @@ static int try_write(uint8_t* stream, uint32_t len){
   uint32_t wlen = len, i;
   uint32_t count = inl(AUDIO_COUNT_ADDR);
   uint32_t size = inl(AUDIO_SBUF_SIZE_ADDR);
+  uint32_t first = inl(AUDIO_START_ADDR);
   uint32_t start = AUDIO_SBUF_ADDR;
   if(count+wlen > size) wlen = size-count;
   // printf("count=%d wlen=%d size=%d\n",count, wlen, size);
   if(wlen == 0) return 0;
   for(i=0; i<wlen; ++i){
-    uint32_t p = start+(i+count)%size;
+    uint32_t p = start+(i+count+first)%size;
     printf("Writing to %u \n",p);
     outb(p, stream[i]);
   }
