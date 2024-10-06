2f2a0aeead61f3959de50d425e23492a50d224b3
tracer-ics2024 <tracer@njuics.org>
2024-10-06 13:23:01 +0800

    >  compile NEMU 231220003 徐梓鸣 Linux carottx-VirtualBox 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:23:01 up  1:34,  1 user,  load average: 0.41, 0.76, 0.88

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -5,8 +5,7 @@
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  int k = AM_KEY_NONE;
-  // int k=inl(0xa0000060);
+  int k=inl(KBD_ADDR);
   kbd->keydown = (k & KEYDOWN_MASK ? true : false);
   kbd->keycode = k & ~KEYDOWN_MASK;
 }
