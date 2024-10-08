047f678313072a171add92765238b118ed54597c
tracer-ics2024 <tracer@njuics.org>
2024-10-08 15:39:27 +0800

    >  compile NEMU 231220003 徐梓鸣 Linux carottx-VirtualBox 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:39:27 up 4 min,  1 user,  load average: 0.71, 0.97, 0.47

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -21,15 +21,15 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   // if (w == 0 || h == 0) return;
-  if (ctl->sync) {  
-    outl(SYNC_ADDR, 1);
-  }
   int ww = io_read(AM_GPU_CONFIG).width;  
   uint32_t* px = ctl->pixels;
   for(int i=0; i<ctl->w; ++i){
     for(int j=0; j<ctl->h; ++j){
       outl((uintptr_t)FB_ADDR+(ww*(ctl->y+j)+ctl->x+i)*sizeof(uint32_t), px[j*ctl->w+i]);
     }
+  }
+  if (ctl->sync) {  
+    outl(SYNC_ADDR, 1);
   }
 }
 
