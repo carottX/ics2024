1b418afe3a2487e9bed54878b3c3a672caf78fce
tracer-ics2024 <tracer@njuics.org>
2024-10-09 23:01:33 +0800

    >  compile NEMU 231220003 徐梓鸣 Linux carottx-VirtualBox 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:01:33 up  4:06,  1 user,  load average: 0.46, 0.36, 0.26

--- abstract-machine/tools/insert-arg.py
+++ abstract-machine/tools/insert-arg.py
@@ -10,7 +10,7 @@ mainargs = argv[4]
 if len(mainargs) >= max_len:
     print("Error: mainargs should not be longer than {0} bytes\n".format(max_len))
     exit(1)
-print("mainargs={0}".format(mainargs))
+#print("mainargs={0}".format(mainargs))
 
 fp = open(bin, 'r+b')
 data = fp.read()
@@ -18,12 +18,10 @@ idx = data.find(str.encode(placeholder))
 if idx == -1:
     print("Error: placeholder not found!\n")
     exit(1)
-print(idx)
 fp.seek(idx)
 mainargs_pad = str.encode(mainargs)+ ((max_len - len(mainargs)) * str.encode("\0"))
 if len(mainargs_pad) != max_len:
     print("Error: len(mainargs_pad) != max_len\n")
     exit(1)
-print('Hello', mainargs_pad)
 fp.write(mainargs_pad)
 fp.close()
