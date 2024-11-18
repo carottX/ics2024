b681229af13b0eacc780a0ab063040a681ca0de4
tracer-ics2024 <tracer@njuics.org>
2024-11-18 21:34:45 +0800

    >  compile NEMU 231220003 徐梓鸣 Linux carottx-ThinkBook 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:34:45 up  1:46,  1 user,  load average: 1.05, 0.89, 0.82

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -14,10 +14,31 @@ size_t ramdisk_read(void *buf, size_t offset, size_t len);
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
   size_t RamSize = get_ramdisk_size();
-  // void* file = malloc(RamSize);
-  // ramdisk_read(file, 0, RamSize);
-  printf("RAMSIZE=%d\n",RamSize);
-  // free(file);
+  void* file = malloc(RamSize);
+  ramdisk_read(file, 0, RamSize);
+  Elf_Ehdr* elf = file;
+  if(elf->e_ident[EI_MAG0] != ELFMAG0 ||
+     elf->e_ident[EI_MAG1] != ELFMAG1 ||
+     elf->e_ident[EI_MAG2] != ELFMAG2 ||
+     elf->e_ident[EI_MAG3] != ELFMAG3){
+    panic("Not a valid elf file!");
+    return (uintptr_t)NULL;
+  }
+  size_t ph_offset = elf->e_phoff;
+  size_t entry_size = elf->e_phentsize;
+  size_t ph_num = elf->e_phnum;
+  for(int i=0; i<ph_num; ++i){
+    Elf_Phdr *seg_header = (Elf_Phdr *)((uint8_t *)elf + ph_offset + i * entry_size);
+    size_t seg_offset = seg_header->p_offset;
+    size_t seg_viraddr = seg_header->p_vaddr;
+    size_t seg_file_size = seg_header->p_filesz;
+    size_t seg_mem_size = seg_header->p_memsz;
+    ramdisk_read(heap.start + seg_viraddr, seg_offset, seg_mem_size);
+    for(int j=seg_file_size; j<seg_mem_size; ++j){
+      *(uint8_t*)(heap.start + seg_viraddr + j) = 0;
+    }
+  }
+  free(file);
   return 0;
 }
 
