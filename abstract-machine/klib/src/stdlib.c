c9ccb39ca3471d489dc986f2771da57cbb475962
tracer-ics2024 <tracer@njuics.org>
2024-11-18 21:37:00 +0800

    >  compile NEMU 231220003 徐梓鸣 Linux carottx-ThinkBook 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:37:00 up  1:48,  1 user,  load average: 0.58, 0.79, 0.80

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -32,7 +32,9 @@ int atoi(const char* nptr) {
 
 void *malloc(size_t size) {
     static size_t tot_size = 0;
-    // printf("%u %u\n",123, 0);
+    printf("tot_size = %u\n", tot_size);
+    printf("0 should be= %u\n", 0);
+
     void* tmp = heap.start + tot_size;
     tot_size += size;
     return tmp;
