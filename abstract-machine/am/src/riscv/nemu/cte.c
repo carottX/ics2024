abe2f23ccbad792932c326ffabcfee150266e35b
tracer-ics2024 <tracer@njuics.org>
2024-11-19 23:22:05 +0800

    >  compile NEMU 231220003 徐梓鸣 Linux carottx-ThinkBook 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:22:05 up 42 min,  1 user,  load average: 0.82, 0.90, 0.78

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -9,7 +9,7 @@ Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
-      // case 8: ev.event = EVENT_SYSCALL;break;
+      case 8: ev.event = EVENT_SYSCALL;break;
       default: ev.event = EVENT_ERROR; break;
     }
     if(c -> gpr[17] == -1) ev.event = EVENT_YIELD;
