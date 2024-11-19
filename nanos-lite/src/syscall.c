db37a5c97ba8e91d085ff9464cea82de7ae33b90
tracer-ics2024 <tracer@njuics.org>
2024-11-19 23:53:51 +0800

    >  compile NEMU 231220003 徐梓鸣 Linux carottx-ThinkBook 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:53:51 up  1:14,  1 user,  load average: 0.54, 0.82, 0.87

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -3,7 +3,7 @@
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
-  printf("SYSCALL ID=%d\n", a[0]);
+  // printf("SYSCALL ID=%d\n", a[0]);
   switch (a[0]) {
     case 0: sys_exit(c); break;
     case 1: sys_yield(c); break;
