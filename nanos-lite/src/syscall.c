5cb2cfa8e7f9792b899b4812b200d1576485203d
tracer-ics2024 <tracer@njuics.org>
2024-11-20 17:11:44 +0800

    >  compile NEMU 231220003 徐梓鸣 Linux carottx-ThinkBook 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:11:44 up 58 min,  1 user,  load average: 0.75, 0.68, 0.66

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,22 +1,11 @@
 #include <common.h>
 #include "syscall.h"
 
-void do_syscall(Context *c) {
-  uintptr_t a[4];
-  a[0] = c->GPR1;
-  #ifdef STRACE
-  printf("SYSCALL ID=%d\n", a[0]);
-  #endif
-  switch (a[0]) {
-    case 0: sys_exit(c); break;
-    case 1: sys_yield(c); break;
-    case 4: sys_write(c); break;
-    default: panic("Unhandled syscall ID = %d", a[0]);
-  }
-  #ifdef STRACE
-  printf("Return Value=%d\n", c->GPRx);
-  #endif
-}
+int fs_open(const char *pathname, int flags, int mode);
+size_t fs_read(int fd, void *buf, size_t len);
+size_t fs_write(int fd, const void *buf, size_t len);
+size_t fs_lseek(int fd, size_t offset, int whence);
+int fs_close(int fd);
 
 void sys_yield(Context *c){
   #ifdef STRACE
@@ -34,26 +23,67 @@ void sys_exit(Context* c){
   halt(c->GPR2);
 }
 
-void sys_write(Context* c){
-  #ifdef STRACE
-  printf("SYSCALL NAME=write\n" );
-  #endif
-  int fd = c->GPR2;
-  // printf("fd = %d len = %d\n", fd, c->GPR4);
-  if(fd == 1 || fd == 2) {
-    uint8_t* buf = (uint8_t*)c->GPR3;
-    int len = c->GPR4;
-    for(int i=0; i<len; ++i) putch(*(buf+i));
-    c->GPRx=len;
-  }
-  else {
-    c->GPRx = -1;
-  }
-}
+// void sys_write(Context* c){
+//   #ifdef STRACE
+//   printf("SYSCALL NAME=write\n" );
+//   #endif
+//   int fd = c->GPR2;
+//   // printf("fd = %d len = %d\n", fd, c->GPR4);
+//   if(fd == 1 || fd == 2) {
+//     uint8_t* buf = (uint8_t*)c->GPR3;
+//     int len = c->GPR4;
+//     for(int i=0; i<len; ++i) putch(*(buf+i));
+//     c->GPRx=len;
+//   }
+//   else {
+//     c->GPRx = -1;
+//   }
+// }
 
 void sys_sbrk(Context* c){
   #ifdef STRACE
   printf("SYSCALL NAME=sbrk\n" );
   #endif
   c->GPRx = 0;
+}
+
+void sys_read(Context* c){
+  c->GPRx = fs_read(c->GPR2, (void *)c->GPR3, c->GPR4);
+}
+
+void sys_write(Context* c){
+  c->GPRx = fs_write(c->GPR2, (void *)c->GPR3, c->GPR4);
+}
+
+void sys_lseek(Context* c){
+  c->GPRx = fs_lseek(c->GPR2, c->GPR3, c->GPR4);
+}
+
+void sys_open(Context* c){
+  c->GPRx = fs_open((void*)c->GPR2, c->GPR3, c->GPR4);
+}
+
+void sys_close(Context* c){
+  c->GPRx = fs_close(c->GPR2);
+}
+
+void do_syscall(Context *c) {
+  uintptr_t a[4];
+  a[0] = c->GPR1;
+  #ifdef STRACE
+  printf("SYSCALL ID=%d\n", a[0]);
+  #endif
+  switch (a[0]) {
+    case 0: sys_exit(c); break;
+    case 1: sys_yield(c); break;
+    case 2: sys_open(c); break;
+    case 3: sys_read(c); break;
+    case 4: sys_write(c); break;
+    case 7: sys_close(c); break;
+    case 8: sys_lseek(c); break;
+    default: panic("Unhandled syscall ID = %d", a[0]);
+  }
+  #ifdef STRACE
+  printf("Return Value=%d\n", c->GPRx);
+  #endif
 }
--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -66,7 +66,7 @@ int _open(const char *path, int flags, mode_t mode) {
 }
 
 int _write(int fd, void *buf, size_t count) {
-  _syscall_(SYS_write, fd, buf, count);
+  _syscall_(SYS_write, fd, (intptr_t)buf, count);
   // _exit(SYS_write);
   return 0;
 }
@@ -80,17 +80,17 @@ void *_sbrk(intptr_t increment) {
 }
 
 int _read(int fd, void *buf, size_t count) {
-  _exit(SYS_read);
+  _syscall_(SYS_open, fd, (intptr_t)buf, count);
   return 0;
 }
 
 int _close(int fd) {
-  _exit(SYS_close);
+  _syscall_(SYS_close, fd, 0, 0);
   return 0;
 }
 
 off_t _lseek(int fd, off_t offset, int whence) {
-  _exit(SYS_lseek);
+  _syscall_(SYS_lseek, fd, offset, whence);
   return 0;
 }
 
