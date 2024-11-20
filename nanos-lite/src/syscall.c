89139e81b1d093480a37fdd3ccd715c545dd5516
tracer-ics2024 <tracer@njuics.org>
2024-11-20 11:30:03 +0800

    >  compile NEMU 231220003 徐梓鸣 Linux carottx-ThinkBook 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:30:03 up 24 min,  1 user,  load average: 0.23, 0.40, 0.36

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,21 +1,50 @@
 #include <common.h>
 #include "syscall.h"
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
-  // printf("SYSCALL ID=%d\n", a[0]);
+  #ifdef STRACE
+  printf("SYSCALL ID=%d\n", a[0]);
+  #endif
   switch (a[0]) {
     case 0: sys_exit(c); break;
     case 1: sys_yield(c); break;
+    case 4: sys_write(c); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
+  #ifdef STRACE
+  printf("Return Value=%d\n", c->GPRx);
+  #endif
 }
 
 void sys_yield(Context *c){
+  #ifdef STRACE
+  printf("SYSCALL NAME=yield\n" );
+  #endif
   yield();
   c -> GPRx = 0;
 }
 
 void sys_exit(Context* c){
-  halt(c->GPR1);
+  #ifdef STRACE
+  printf("SYSCALL NAME=exit\n" );
+  #endif
+  halt(c->GPR2);
+}
+
+void sys_write(Context* c){
+  #ifdef STRACE
+  printf("SYSCALL NAME=write\n" );
+  #endif
+  int fd = c->GPR1;
+  if(fd == 1 || fd == 2) {
+    uint8_t* buf = (uint8_t*)c->GPR2;
+    int len = c->GPR3;
+    for(int i=0; i<len; ++i) putch(*(buf+i));
+    c->GPRx=len;
+  }
+  else {
+    c->GPRx = -1;
+  }
 }
