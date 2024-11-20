ad4f63c6a703fecacd861bf283bba5e32d31a327
tracer-ics2024 <tracer@njuics.org>
2024-11-20 11:39:03 +0800

    >  compile NEMU 231220003 徐梓鸣 Linux carottx-ThinkBook 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:39:03 up 33 min,  1 user,  load average: 0.98, 0.90, 0.65

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -30,6 +30,7 @@ void sys_exit(Context* c){
   #ifdef STRACE
   printf("SYSCALL NAME=exit\n" );
   #endif
+  printf("exit=%d\n", c->GPR2);
   halt(c->GPR2);
 }
 
@@ -38,7 +39,7 @@ void sys_write(Context* c){
   printf("SYSCALL NAME=write\n" );
   #endif
   int fd = c->GPR2;
-  printf("fd = %d len = %d\n", fd, c->GPR4);
+  // printf("fd = %d len = %d\n", fd, c->GPR4);
   if(fd == 1 || fd == 2) {
     uint8_t* buf = (uint8_t*)c->GPR3;
     int len = c->GPR4;
