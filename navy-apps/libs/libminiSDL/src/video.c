d5da125b762eddc0eb8fa46c10d21fe8d7358ff3
tracer-ics2024 <tracer@njuics.org>
2024-11-21 21:52:50 +0800

    >  compile NEMU 231220003 徐梓鸣 Linux carottx-ThinkBook 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:52:50 up  3:10,  1 user,  load average: 2.04, 1.74, 1.62

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -7,6 +7,7 @@
 void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_Rect *dstrect) {
   assert(dst && src);
   assert(dst->format->BitsPerPixel == src->format->BitsPerPixel);
+  printf("??");
   int dest_x = 0, dest_y = 0;
   if(dstrect != NULL) dest_x = dstrect->x, dest_y = dstrect->y;
   int start_x = 0, start_y = 0;
