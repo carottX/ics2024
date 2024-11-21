b8dcc37a856ddad4ffa85bc37e2a4177c269bc9c
tracer-ics2024 <tracer@njuics.org>
2024-11-21 21:49:21 +0800

    >  compile NEMU 231220003 徐梓鸣 Linux carottx-ThinkBook 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:49:21 up  3:06,  1 user,  load average: 1.79, 1.59, 1.57

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -20,7 +20,7 @@ int SDL_PollEvent(SDL_Event *ev) {
 int SDL_WaitEvent(SDL_Event *event) {
   char buf[16];
   while(NDL_PollEvent(buf, 16) == 0);
-  puts(buf);
+  printf(buf);
   if(strncmp(buf, "kd", 2) == 0) event->type = SDL_KEYDOWN;
   else event->type = SDL_KEYUP;
   for(int i = 0; i < sizeof(keyname)/sizeof(keyname[0]); ++i){
