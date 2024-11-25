c0c7192676a16d180cc84c2f10e8e9ad38e7f21c
tracer-ics2024 <tracer@njuics.org>
2024-11-25 21:49:18 +0800

    >  compile NEMU 231220003 徐梓鸣 Linux carottx-ThinkBook 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:49:18 up  2:21,  1 user,  load average: 0.83, 0.74, 0.66

--- navy-apps/libs/libSDL_image/src/image.c
+++ navy-apps/libs/libSDL_image/src/image.c
@@ -2,6 +2,8 @@
 #define SDL_free    free
 #define SDL_realloc realloc
 
+#include <unistd.h>
+
 #define SDL_STBIMAGE_IMPLEMENTATION
 #include "SDL_stbimage.h"
 
@@ -12,7 +14,25 @@ SDL_Surface* IMG_Load_RW(SDL_RWops *src, int freesrc) {
 }
 
 SDL_Surface* IMG_Load(const char *filename) {
-  return NULL;
+  int fd = open(filename);
+  if(fd == -1){
+    printf("Error when opening the file!\n");
+    return NULL;
+  }
+  int sz = lseek(fd, 0, SEEK_END);
+  if(sz == -1){
+    printf("Error when getting the file size!\n");
+    return NULL;
+  }
+  void* buf = SDL_malloc(sz);
+  if(read(fd, buf, sz) == -1) {
+    printf("Error when reading the file!\n");
+    return NULL;
+  }
+  SDL_Surface* ret = STBIMG_LoadFromMemory(buf, sz);
+  close(fd);
+  free(buf);
+  return ret;
 }
 
 int IMG_isPNG(SDL_RWops *src) {
