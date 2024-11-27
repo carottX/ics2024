01b86b8bfcdde1bab05b3f7b38cedeb720b210b1
tracer-ics2024 <tracer@njuics.org>
2024-11-27 22:16:32 +0800

    >  compile NEMU 231220003 徐梓鸣 Linux carottx-ThinkBook 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:16:32 up 13 min,  1 user,  load average: 1.63, 1.11, 0.60

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -16,18 +16,16 @@ int SDL_PushEvent(SDL_Event *ev) {
 int SDL_PollEvent(SDL_Event *ev) {
   // printf("POLL BEGIN\n");
   char buf[16];
-  if(NDL_PollEvent(buf, 16) == 0) {    printf("POLL FINI\n");
-return 0;}
+  if(NDL_PollEvent(buf, 16) == 0) return 0;
   if(strncmp(buf, "kd", 2) == 0) ev->type = SDL_KEYDOWN;
   else ev->type = SDL_KEYUP;
   for(int i = 0; i < sizeof(keyname)/sizeof(keyname[0]); ++i){
     if(strncmp(keyname[i], buf+3, strlen(keyname[i])) == 0){
-      ev -> key.keysym.sym = i;    printf("POLL FINI\n");
-
+      ev -> key.keysym.sym = i;
       return 1;
     }
   }
-    printf("POLL FINI\n");
+    // printf("POLL FINI\n");
 
   return 0;
 }
@@ -53,13 +51,13 @@ int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
 }
 
 uint8_t* SDL_GetKeyState(int *numkeys) {
-  printf("HELLO?\n");
+  // printf("HELLO?\n");
   size_t sz = sizeof(keyname) / sizeof(keyname[0]);
   uint8_t* ret = malloc(sizeof(uint8_t) * (numkeys == NULL ? sz : *numkeys));
   SDL_Event tmp;
   SDL_PollEvent(&tmp);
   for(int i=0; i<sz; ++i) if(tmp.key.keysym.sym == i && tmp.type == SDL_KEYDOWN) ret[i] = 1; else ret[i] = 0;
-  if(ret == NULL) printf("WTF?\n");
+  // if(ret == NULL) printf("WTF?\n");
 
   return ret;
 }
