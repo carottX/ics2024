db1980f659bf763277b11d84e008c0f5d675b5fd
tracer-ics2024 <tracer@njuics.org>
2024-11-30 16:49:44 +0800

    >  compile NEMU 231220003 徐梓鸣 Linux carottx-ThinkBook 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:49:44 up  4:11,  1 user,  load average: 2.03, 1.57, 1.30

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -68,7 +68,7 @@ int fs_open(const char *pathname, int flags, int mode){
 size_t fs_read(int fd, void *buf, size_t len){
   static int counter = 0;
   printf("FSREAD fd=%d len=%d\n count = %d\n",fd,len,counter);
-  counter++;
+  if(fd == 64)counter++;
   ReadFn ReadFunc = ramdisk_read;
   if(file_table[fd].read != NULL) ReadFunc = file_table[fd].read;
   else len = min(len, (int)file_table[fd].size - (int)file_table[fd].p_offset);
