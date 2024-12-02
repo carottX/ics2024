373cfa908c232ba595e05c221d4af2836ba042b2
tracer-ics2024 <tracer@njuics.org>
2024-12-02 21:45:59 +0800

    >  compile NEMU 231220003 徐梓鸣 Linux carottx-ThinkBook 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:45:59 up  2:18,  1 user,  load average: 1.13, 1.22, 1.26

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -16,7 +16,7 @@ void sys_execve(Context* c){
   #ifdef STRACE
   printf("SYSCALL NAME=execve\n" );
   #endif
-  printf("name=%s\n",(const char*)c->GPR2);
+  printf("TRIED? name=%s\n",(const char*)c->GPR2);
   naive_uload(NULL, (const char*)c->GPR2);
 }
 
