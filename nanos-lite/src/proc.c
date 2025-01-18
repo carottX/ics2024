7b03f0f79475712d54c0ae5a03bc0ed3177154ce
tracer-ics2024 <tracer@njuics.org>
2025-01-18 16:43:05 +0800

    >  compile NEMU 231220003 徐梓鸣 Linux carottx-ThinkBook 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:43:05 up 59 min,  1 user,  load average: 1.01, 0.65, 0.49

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -8,6 +8,8 @@ PCB *current = NULL;
 
 void naive_uload(PCB *pcb, const char *filename);
 
+void context_uload(PCB *pcb, const char *filename);
+
 void switch_boot_pcb() {
   current = &pcb_boot;
 }
@@ -27,7 +29,7 @@ void context_kload(PCB* pcb, void(*entry)(void *), void *arg) {
 
 void init_proc() {
   context_kload(&pcb[0], hello_fun, (void *)0);
-  context_kload(&pcb[1], hello_fun, (void *)1);
+  context_uload(&pcb[1], "/bin/pal");
   switch_boot_pcb();
 
   yield();
