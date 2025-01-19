dd39ec9e8e9cec40c4d8dfaad2bd1db543bf1305
tracer-ics2024 <tracer@njuics.org>
2025-01-19 10:54:35 +0800

    >  compile NEMU 231220003 徐梓鸣 Linux carottx-ThinkBook 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:54:35 up 42 min,  1 user,  load average: 0.24, 0.48, 0.43

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -7,14 +7,14 @@ int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
 void __libc_init_array (void);
 void call_main(uintptr_t *args) {
-  printf("args=%p\n",args);
+  // printf("args=%p\n",args);
   int argc = (int)args[0];
-  printf("argc=%d\n",argc);
+  // printf("argc=%d\n",argc);
   char** argv = (char **)(args+1);
   char** envp = (char **)(args + argc + 1);
-  for(int i=0; i<argc; ++i){
-    printf("argv[%d]=%s\n",i,argv[i]);
-  }
+  // for(int i=0; i<argc; ++i){
+  //   printf("argv[%d]=%s\n",i,argv[i]);
+  // }
   environ = envp;
   __libc_init_array();
   exit(main(argc, argv, envp));
