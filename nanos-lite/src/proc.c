368f9e28109e9fa768b03796f30aa4bc1f73e9be
tracer-ics2024 <tracer@njuics.org>
2025-01-19 11:09:54 +0800

    >  compile NEMU 231220003 徐梓鸣 Linux carottx-ThinkBook 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:09:54 up 57 min,  1 user,  load average: 0.99, 0.94, 0.76

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -29,9 +29,9 @@ void context_kload(PCB* pcb, void(*entry)(void *), void *arg) {
 
 void init_proc() {
   context_kload(&pcb[0], hello_fun, (void *)0);
-  char* const argv[] = {"/bin/nterm", NULL};
+  char* const argv[] = {"/bin/busybox", NULL};
   char* const envp[] = {NULL};
-  context_uload(&pcb[1], "/bin/nterm", argv, envp);
+  context_uload(&pcb[1], "/bin/busybox", argv, envp);
   switch_boot_pcb();
 
   yield();
