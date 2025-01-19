2b904d4ffb2a47a3c644d08bf24ca1bd2b3c2c94
tracer-ics2024 <tracer@njuics.org>
2025-01-19 11:52:13 +0800

    >  compile NEMU 231220003 徐梓鸣 Linux carottx-ThinkBook 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:52:13 up  1:39,  1 user,  load average: 0.52, 0.33, 0.23

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -71,8 +71,8 @@ void context_uload(PCB* pcb, const char *filename, char* const argv[], char* con
   while(argv[argc] != NULL) argc++;
   int envc = 0;
   while(envp[envc] != NULL) envc++;
-  // printf("argc=%d envc=%d\n",argc,envc);
-  // for(int i=0; i<argc; ++i) printf("argv[%d]=%s\n",i,argv[i]);
+  printf("argc=%d envc=%d\n",argc,envc);
+  for(int i=0; i<argc; ++i) printf("argv[%d]=%s\n",i,argv[i]);
   uintptr_t argv_pos[argc], envp_pos[envc];
   char* stk = (char*)new_page(8) + PGSIZE * 8;
   // printf("stk=%p\n",stk);
