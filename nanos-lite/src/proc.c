f8663864913d3c05e13e0907cba9dc43e1694d8d
tracer-ics2024 <tracer@njuics.org>
2025-01-19 10:59:08 +0800

    >  compile NEMU 231220003 徐梓鸣 Linux carottx-ThinkBook 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:59:08 up 46 min,  1 user,  load average: 0.68, 0.75, 0.57

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -17,7 +17,7 @@ void switch_boot_pcb() {
 void hello_fun(void *arg) {
   int j = 1;
   while (1) {
-    if(j%1000 == 0)Log("Hello World from Nanos-lite with arg '%p' for the %dth time!", (uintptr_t)arg, j);
+    if(j%100 == 0)Log("Hello World from Nanos-lite with arg '%p' for the %dth time!", (uintptr_t)arg, j);
     j ++;
     yield();
   }
