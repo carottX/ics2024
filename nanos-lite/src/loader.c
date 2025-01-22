c0308c2977dba0edd6dc39d5af5c043899a93a7e
tracer-ics2024 <tracer@njuics.org>
2025-01-22 14:20:55 +0800

    >  compile NEMU 231220003 徐梓鸣 Linux carottx-ThinkBook 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:20:55 up  4:31,  1 user,  load average: 0.84, 0.89, 0.82

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -23,7 +23,7 @@ size_t ramdisk_read(void *buf, size_t offset, size_t len);
 static void* allocate(AddrSpace* as, uintptr_t vaddr, size_t p_memsz){
   vaddr = ROUNDDOWN(vaddr, PGSIZE);
   size_t new_page_num = ((vaddr + p_memsz - 1)/PGSIZE) - (vaddr/PGSIZE) + 1;
-  printf("Loaded sements from [%p, %p]\n",vaddr, vaddr + p_memsz);
+  printf("Loaded segments from [%p, %p]\n",vaddr, vaddr + p_memsz);
   void* ret = new_page(new_page_num);
   for(int i=0; i<=new_page_num; ++i){
     map(as, (void*)vaddr + i*PGSIZE, ret + i*PGSIZE, 0);
@@ -51,7 +51,6 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
     fs_lseek(fd, ph_offset+entry_size*i, 0);
     fs_read(fd, &seg_header, entry_size);
     if(seg_header.p_type != PT_LOAD) continue;
-    // printf("LOADED!\n");
     size_t seg_offset = seg_header.p_offset;
     uintptr_t seg_viraddr = seg_header.p_vaddr;
     size_t seg_file_size = seg_header.p_filesz;
