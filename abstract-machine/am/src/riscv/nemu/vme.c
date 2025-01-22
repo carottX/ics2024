10b31909ef2dd88e8835154e48927a7b6968c6c3
tracer-ics2024 <tracer@njuics.org>
2025-01-22 20:24:34 +0800

    >  compile NEMU 231220003 徐梓鸣 Linux carottx-ThinkBook 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:24:34 up 10:35,  1 user,  load average: 1.19, 0.77, 0.60

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -86,5 +86,6 @@ Context *ucontext(AddrSpace *as, Area kstack, void *entry) {
   Context* c = (Context*)(kstack.end - sizeof(Context));
   c->mepc = (intptr_t)entry;
   c->pdir = as->ptr;
+  c->mstatus = 0x80; // MPIE
   return c;
 }
