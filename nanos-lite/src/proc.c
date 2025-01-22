88b8e394be969bc4a041eb53306c6429d6ef5727
tracer-ics2024 <tracer@njuics.org>
2025-01-22 17:41:07 +0800

    >  compile NEMU 231220003 徐梓鸣 Linux carottx-ThinkBook 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:41:07 up  7:51,  1 user,  load average: 1.15, 0.73, 0.53

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -30,13 +30,13 @@ void context_kload(PCB* pcb, void(*entry)(void *), void *arg) {
 #define PAL_NAME "/bin/pal"
 
 void init_proc() {
-  // char* const argv[] = {PAL_NAME, NULL};
-  // char* const envp[] = {NULL};
+  char* const argv[] = {PAL_NAME, NULL};
+  char* const envp[] = {NULL};
   char* const argv2[] = {"/bin/hello", NULL};
   char* const envp2[] = {NULL};
   context_uload(&pcb[1], "/bin/hello", argv2, envp2);
   // context_uload(&pcb[1], PAL_NAME, argv, envp);
-  context_kload(&pcb[0], hello_fun, "TWO");
+  context_uload(&pcb[0], PAL_NAME, argv, envp);
   // context_kload(&pcb[1], hello_fun, "ONE");
   switch_boot_pcb();
 
