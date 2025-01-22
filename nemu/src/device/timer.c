1e34c4bb1179868f92feeaeb43d0676042e77b29
tracer-ics2024 <tracer@njuics.org>
2025-01-22 20:23:38 +0800

    >  compile NEMU 231220003 徐梓鸣 Linux carottx-ThinkBook 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:23:38 up 10:34,  1 user,  load average: 0.72, 0.60, 0.54

--- nemu/src/device/timer.c
+++ nemu/src/device/timer.c
@@ -32,8 +32,6 @@ static void rtc_io_handler(uint32_t offset, int len, bool is_write) {
 static void timer_intr() {
   if (nemu_state.state == NEMU_RUNNING) {
     extern void dev_raise_intr();
-      printf("timer intr\n");
-
     dev_raise_intr();
   }
 }
